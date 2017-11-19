0353250dc042eb2ad5b0ec481ff8a363e7bbd179
Asela Dassanayake <asela.d82@gmail.com>
2017-11-18 18:13:41 -0600

    Update config.py

--- API challenges/config.py
+++ API challenges/config.py
@@ -1,4 +1,2 @@
-nyt_api_key = "d4bf3a7a0f504a0cb4b741700c5ea656"
-omdb_api_key = "10abc16f"
+
 openweather_api_key = "50e3d73beef796a3c10e716909fb6a2a"
-google_places_api_key = "AIzaSyBPtERPD47WmGx7r91ibrHiC_lRpcT99Xo"
